e719ee775ff288f517ec7c68a5ef06c1d6205e3e
Daniel Fakunle <daniel@Daniels-MacBook-Pro.local>
2026-06-22 13:46:01 -0500

    ci: add workflows

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,23 @@ export default defineConfig({
   },
   run: {
     cache: true,
+    tasks: {
+      "act:ci:dry": {
+        command:
+          "act -n pull_request -W .github/workflows/ci.yml -e .github/workflows/pr-opened.json",
+      },
+      "act:ci": {
+        command:
+          "act pull_request -W .github/workflows/ci.yml -e .github/workflows/pr-opened.json -P ubuntu-latest=catthehacker/ubuntu:act-latest",
+      },
+
+      "act:release:dry": {
+        command: "act -n push -W .github/workflows/release.yml",
+      },
+      "act:release": {
+        command:
+          "act push -W .github/workflows/release.yml -P ubuntu-latest=catthehacker/ubuntu:act-latest",
+      },
+    },
   },
 });
